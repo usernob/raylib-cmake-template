32654af417a5a6a4936ba2118c5937e57a453230
usernob <shamsulmaarif2016@gmail.com>
2025-01-01 10:40:03 +0700

    add formatting stuff

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    Texture2D texture = LoadTexture(ASSETS_PATH "test.png"); // Check README.md for how this works
 
     while (!WindowShouldClose())
     {
@@ -22,7 +22,7 @@ int main(void)
         const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
+        const char *text = "OMG! IT WORKS!";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
